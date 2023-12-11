832eb1889eaaf62ac1d3c7c0c347b6589c4dba22
Vincent MD <100425293+RepliKode@users.noreply.github.com>
2023-12-11 20:10:42 +0100

    Update youtube-video-looper.user.js

--- youtube-video-looper.user.js
+++ youtube-video-looper.user.js
@@ -16,15 +16,15 @@
     let loopInterval;
     let loopStartSeconds;
     let loopEndSeconds;
-    let videoId; // Déclarer la variable en dehors du bloc if
+    let videoId; // Declare the variable outside the if block
 
-    // Vérifier si la page actuelle est une vidéo YouTube ou YouTube Short
+    // Check if the current page is a YouTube or YouTube Short video
     if (isYouTubeVideoPage()) {
-        // Récupérer l'ID de la vidéo à partir de l'URL
+        // Get the video ID from the URL
         const videoIdMatch = window.location.search.match(/(?:v=|\/shorts\/)([^&/]+)/);
         videoId = videoIdMatch ? videoIdMatch[1] : null;
 
-        // Vérifier si la vidéo a déjà une boucle définie
+        // Check if the video already has a defined loop
         const storedLoopStart = localStorage.getItem(`loopStart_${videoId}`);
         const storedLoopEnd = localStorage.getItem(`loopEnd_${videoId}`);
 
@@ -35,79 +35,79 @@
         }
     }
 
-    // Fonction pour vérifier si la page actuelle est une vidéo YouTube ou YouTube Short
+    // Function to check if the current page is a YouTube or YouTube Short video
     function isYouTubeVideoPage() {
         return window.location.href.includes('/watch?v=') || window.location.href.includes('/shorts/');
     }
 
     function startLoop() {
-        // Attendre que le lecteur vidéo soit prêt
+        // Wait for the video player to be ready
         player = document.querySelector('video');
         if (player) {
             player.currentTime = loopStartSeconds;
 
-            // Lancer la lecture
+            // Start playing
             player.play();
 
-            // Vérifier le temps périodiquement
+            // Check the time periodically
             loopInterval = setInterval(checkTime, 500);
         } else {
-            setTimeout(startLoop, 500); // Attendre et réessayer
+            setTimeout(startLoop, 500); // Wait and try again
         }
     }
 
     function checkTime() {
         if (player.currentTime >= loopEndSeconds) {
-            // Revenir au point de départ à la fin de la section
+            // Go back to the starting point at the end of the section
             player.currentTime = loopStartSeconds;
         }
     }
 
     function resetVideoLooper() {
-        // Si la boucle est déjà définie, réinitialiser la vidéo à la boucle
+        // If the loop is already defined, reset the video to the loop
         if (loopStartSeconds !== undefined && loopEndSeconds !== undefined) {
-            // Supprimer la boucle en réinitialisant les valeurs
+            // Remove the loop by resetting the values
             loopStartSeconds = undefined;
             loopEndSeconds = undefined;
             localStorage.removeItem(`loopStart_${videoId}`);
             localStorage.removeItem(`loopEnd_${videoId}`);
-            clearInterval(loopInterval); // Arrêter la boucle
-            player.play(); // Reprendre la lecture
+            clearInterval(loopInterval); // Stop the loop
+            player.play(); // Resume playback
         } else {
-            // Si la boucle n'est pas définie, demander à l'utilisateur
-            const userInput = prompt('Entrez le temps de début et de fin (format hhmmss hhmmss) :');
+            // If the loop is not defined, ask the user
+            const userInput = prompt('Enter the start and end time (format hhmmss hhmmss):');
 
-            if (userInput !== null) { // Si l'utilisateur n'a pas cliqué sur "Annuler"
+            if (userInput !== null) { // If the user didn't click "Cancel"
                 const inputParts = userInput.split(' ');
                 if (inputParts.length === 2) {
                     loopStartSeconds = timeStringToSeconds(inputParts[0] + '00');
                     loopEndSeconds = timeStringToSeconds(inputParts[1] + '00');
 
-                    // Enregistrer la boucle dans le stockage local pour une utilisation future
+                    // Save the loop in local storage for future use
                     localStorage.setItem(`loopStart_${videoId}`, loopStartSeconds);
                     localStorage.setItem(`loopEnd_${videoId}`, loopEndSeconds);
 
-                    // Commencer la boucle
+                    // Start the loop
                     startLoop();
                 } else {
-                    alert('Format invalide. Utilisez le format "hhmmss hhmmss".');
+                    alert('Invalid format. Use the format "hhmmss hhmmss".');
                 }
             }
         }
 
-        // Mettre à jour le texte du bouton en fonction de la présence ou de l'absence de boucle
+        // Update the button text based on the presence or absence of a loop
         updateButtonLabel();
     }
 
-    // Fonction pour mettre à jour le texte du bouton en fonction de la présence ou de l'absence de boucle
+    // Function to update the button text based on the presence or absence of a loop
     function updateButtonLabel() {
         const resetButton = document.getElementById('resetButton');
         if (resetButton) {
-            resetButton.textContent = loopStartSeconds !== undefined ? 'Réinitialiser le Looper' : 'Initialiser le Looper';
+            resetButton.textContent = loopStartSeconds !== undefined ? 'Reset Looper' : 'Initialize Looper';
         }
     }
 
-    // Créez un bouton de réinitialisation
+    // Create a reset button
     const resetButton = document.createElement('button');
     resetButton.id = 'resetButton';
     resetButton.style.position = 'fixed';
@@ -116,13 +116,13 @@
     resetButton.style.zIndex = '1000';
     resetButton.addEventListener('click', resetVideoLooper);
 
-    // Ajoutez le bouton à la page
+    // Add the button to the page
     document.body.appendChild(resetButton);
 
-    // Initialise le texte du bouton au chargement de la page
+    // Initialize the button text on page load
     updateButtonLabel();
 
-    // Convertir une chaîne de temps au format hhmmss en secondes
+    // Convert a time string in the format hhmmss to seconds
     function timeStringToSeconds(timeString) {
         const hours = parseInt(timeString.substring(0, 2)) || 0;
         const minutes = parseInt(timeString.substring(2, 4)) || 0;
